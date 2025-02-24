a590922f4ad95cf3cc9a414d5c99c9f093fb9e78
jacobgoodboy <jacobgoodboy9@gmail.com>
2025-02-24 04:33:38 -0600

    declared routes and installed dependancies for backend and added dotenv to ignore modules

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
+import authRoutes from './routes/auth.route'
+
 import express from 'express'
 const app = express()
+
+app.use("/api/auth",authRoutes)
 
 app.listen(3000, ()=>{
     console.log("server is running on port 3000")
