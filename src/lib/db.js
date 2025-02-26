7c4c1ac06c749910370ff7d98f3a39ee94d30e72
jacobgoodboy <jacobgoodboy9@gmail.com>
2025-02-26 05:22:43 -0600

    added user schema and model

--- src/lib/db.js
+++ src/lib/db.js
@@ -4,7 +4,7 @@ import 'dotenv/config';
 export const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGODB_URI)
-        console.log(`connected to: ${conn.connection.host}`)
+        console.log(`connected successfully to: ${conn.connection.host}`)
     } catch (err) {
         console.log("MongoDB Error:", err)
     }
