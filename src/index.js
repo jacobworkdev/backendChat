cf4a86f8cd4ca4eaeb491a49916e1288b970dee7
jacobgoodboy <jacobgoodboy9@gmail.com>
2025-02-27 03:41:09 -0600

    added cors

--- src/index.js
+++ src/index.js
@@ -4,16 +4,21 @@ import 'dotenv/config';
 import { connectDB } from './lib/db.js';
 import cookieParser from "cookie-parser"
 import messageRoutes from "./routes/message.routes.js"
-const PORT=process.env.PORT
+
+import cors from 'cors'
+const PORT = process.env.PORT
 
 //EXPRESS
 const app = express()
 app.use(express.json())
 app.use(cookieParser())
-app.use("/api/auth",authRoutes)
-app.use('/api/message',messageRoutes)
+app.use(cors(
+    { origin: "http://localhost:5173",credentials:true }
+))
+app.use("/api/auth", authRoutes)
+app.use('/api/message', messageRoutes)
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`server is running on port:${PORT}`)
     connectDB()
 })
